5dbd4a5d705280ee9eab2928c4c6b0da7330e4b1
Joao Vitor Oliveira <joaovitorgbj1@gmail.com>
2026-04-11 16:57:47 -0300

    Finalizado

--- cadastroProdutos.py
+++ cadastroProdutos.py
@@ -1,4 +1,3 @@
-produtos = []
 
 def listarProdutos():
     
@@ -32,8 +31,87 @@ def removerProduto (indice):
          return True
      return False
      
-        
+produtos = [
+     {"nome": "Arroz", "preco": 20.00},
+    {"nome": "Feijão", "preco": 10.00},
+    {"nome": "Farofa", "preco": 7.00},
+    {"nome": "Brocolis", "preco": 6.00},
+    {"nome": "Cereal", "preco": 8.00},
+] 
 
+opcao = None
+while opcao != "0":
+    print()
+    print("========================================")
+    print("               MENU")
+    print("========================================")
+    print("1 - Listar Produtos")
+    print("2 - Adicionar Produto")
+    print("3 - Buscar Produto")
+    print("4 - Atualizar Produto")
+    print("5 - Remover Produto")
+    print("0 - Sair")
+    print("========================================")
 
+    if opcao == "1":
+        print()
+        print("LISTA DE PRODUTOS ======================")
+        listarProdutos()
 
+    elif opcao == "2":
+        print()
+        print("ADICIONAR DE PRODUTOS ==================")
+        nome = input("Nome:")
+        preco = float(input("Preço:"))
+        if adicionarProduto({"nome": nome, "preco": preco}):
+            print()
+            print("LISTA DE PRODUTOS ======================")
+            listarProdutos()
+            print("Produto adicionado com sucesso")
+        else:
+            print("Erro ao cadastrar produto")
 
+    elif opcao == "3":
+        print()
+        print("BUSCAR PRODUTO =========================")
+        busca = input("Informe o nome do produto: ")
+        produtoEncontrado = buscarProduto(busca)
+
+        if produtoEncontrado == None:
+            print("Produto não encontrado")
+        else:
+            print("Produto encontrado", produtos[produtoEncontrado])
+
+    elif opcao == "4":
+        print("ATUALIZAR PRODUTO ======================")
+        print()
+        busca = input("Informe o nome do produto: ")
+        produtoEncontrado = buscarProduto(busca)
+        if produtoEncontrado == None:
+            print("Produto não encontrado")
+        else:
+            nome = input("nome: ")
+            preco = float(input("preco: "))
+            atualizarProduto(produtoEncontrado, {"nome": nome, "preco": preco})
+        print()
+        listarProdutos()
+
+    elif opcao == "5":
+        print("REMOVER PRODUTO ========================")
+        print()
+        remover = input("informe o produto para remover: ")
+        produtoEncontrado = buscarProduto(remover)
+        if produtoEncontrado == None:
+            print("Produto não encontrado")
+        else:
+            removerProduto(produtoEncontrado)
+            print("produto removido com sucesso")
+
+        print()
+        listarProdutos()
+
+    elif opcao != None:
+        print("Opção não existe")
+
+    print()
+    opcao = input("Opção desejada:")
